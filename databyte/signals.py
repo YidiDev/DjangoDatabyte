8351b738d60f3f8681a56c2f627439b6dfdcc2b5
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-15 21:03:39 -0400

    updating signals.py to account for file storage

--- databyte/signals.py
+++ databyte/signals.py
@@ -1,15 +1,30 @@
-from django.db.models.signals import post_save
+from django.db import models
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from databyte.utils import compute_instance_storage, compute_child_storage, compute_external_storage, \
+    compute_file_fields_storage
 
 
-@receiver(post_save)
+# noinspection PyProtectedMember
+@receiver([post_save, post_delete])
 def update_storage(sender, instance, **kwargs):
     # Check if the instance has an AutomatedStorageTrackingField
     if hasattr(instance, 'AutomatedStorageTrackingField'):
-        # Compute the storage here and update the instance's value
-        # (Consider recursively checking children records as described)
+        # Compute the instance's storage, child storage, and external storage
+        instance_storage = compute_instance_storage(instance)
+        child_storage = compute_child_storage(instance)
+        external_storage = compute_external_storage(instance)
+        file_storage = compute_file_fields_storage(instance)
 
-        # Update parent records if include_in_parents_count is True
+        # Update the instance's AutomatedStorageTrackingField value
+        instance.AutomatedStorageTrackingField = instance_storage + child_storage + external_storage + file_storage
+        instance.save(update_fields=['AutomatedStorageTrackingField'])
+
+        # Signal parent records to update if required
         if instance.AutomatedStorageTrackingField.include_in_parents_count:
-            # Find parent records and force them to update their count
-            pass
+            for field in instance._meta.fields:
+                if isinstance(
+                        field, models.ForeignKey
+                ) and hasattr(field.related_model, 'AutomatedStorageTrackingField'):
+                    parent = getattr(instance, field.name)
+                    parent.save()
