ffa4df8b6dfdd196c6c5bafbfb75eb93943bb94a
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-15 22:26:12 -0400

    removing notes

--- databyte/signals.py
+++ databyte/signals.py
@@ -8,19 +8,15 @@ from databyte.utils import compute_instance_storage, compute_child_storage, comp
 # noinspection PyProtectedMember
 @receiver([post_save, post_delete])
 def update_storage(sender, instance: models.Model, **kwargs) -> None:
-    # Check if the instance has an AutomatedStorageTrackingField
     if hasattr(instance, 'AutomatedStorageTrackingField'):
-        # Compute the instance's storage, child storage, and external storage
         instance_storage: int = compute_instance_storage(instance)
         child_storage: int = compute_child_storage(instance)
         external_storage: int = compute_external_storage(instance)
         file_storage: int = compute_file_fields_storage(instance)
 
-        # Update the instance's AutomatedStorageTrackingField value
         instance.AutomatedStorageTrackingField = instance_storage + child_storage + external_storage + file_storage
         instance.save(update_fields=['AutomatedStorageTrackingField'])
 
-        # Signal parent records to update if required
         if instance.AutomatedStorageTrackingField.include_in_parents_count:
             for field in instance._meta.fields:
                 if isinstance(
