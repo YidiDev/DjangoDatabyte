ae724e722e35ea9fdb7d37a252f5fb8d76dfea8a
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-15 22:33:47 -0400

    adding storage aware foreign key

--- databyte/fields.py
+++ databyte/fields.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class StorageAwareForeignKey(models.ForeignKey):
+    def __init__(self, *args, count_as_storage_parent=False, **kwargs):
+        self.count_as_storage_parent = count_as_storage_parent
+        super().__init__(*args, **kwargs)
+
+
 class ExternalStorageTrackingField(models.BigIntegerField):
     def __init__(self, *args, **kwargs):
         kwargs['default'] = 0
