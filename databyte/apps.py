627c05081f6d2bb5d568c12bfeaad32b8bb5547e
Yidi Sprei <yidi.sprei@yidienterprises.com>
2023-08-15 21:10:55 -0400

    updating apps.py

--- databyte/apps.py
+++ databyte/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class DatabyteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'databyte'
+    default_auto_field: str = 'django.db.models.BigAutoField'
+    name: str = 'databyte'
+    verbose_name: str = 'Databyte'
